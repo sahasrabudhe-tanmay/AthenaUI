a6942efb976823926ebf8f3558669b7d550c6816
Tanmay Sahasrabudhe <tanmay.sahasrabudhe@gmail.com>
2020-05-02 21:44:13 +0530

    Navbar is responsive

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import './App.css';
 import Login from './components/login/Login';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Register from './components/register/Register';
 import Navbar from './components/navbar/Navbar';
+import Boards from './components/boards/Boards';
+import Home from './components/home/Home';
+import Issues from './components/issues/Issues';
 
 export default class App extends Component {
 
@@ -27,8 +30,13 @@ export default class App extends Component {
     return (
       <Router>
         <Navbar />
-        <Route path="/login" render={() => <Login setUser={this.setUser} />} />
-        <Route path="/register" render={() => <Register setUser={this.setUser} />} />
+        <Switch>
+          <Route path="/home" component={Home} />
+          <Route path="/boards" component={Boards} />
+          <Route path="/issues" component={Issues} />
+          <Route path="/login" render={() => <Login setUser={this.setUser} />} />
+          <Route path="/register" render={() => <Register setUser={this.setUser} />} />
+        </Switch>
       </Router>
     );
   }
---
+++ src/components/boards/Boards.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Boards extends Component {
+    render() {
+        return (
+            <div className="white">
+                Boards works!
+            </div>
+        )
+    }
+}
+
